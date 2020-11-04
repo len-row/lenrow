8c4016029faa7c35ceaca2098bd2704b5ff29702
Lennard Rowlands <26552689+len-row@users.noreply.github.com>
2020-11-04 18:33:16 +0100

    uploaded intial version
    initial landing page

---
+++ resources/main.js
@@ -0,0 +1,28 @@
+const d = document
+
+forwardFunctions = {
+    1:"https://t.me/lennardrowlands",
+    2:"https://mailto:contact@lenrow.dev",
+    3:"https://github.com/len-row",
+    4:"infoBox(1)",
+    5:"infobox(2)"
+}
+
+function forwardMe(i) {
+    if (i === 4 || i === 5) {
+        infoBox(i)
+        return
+    } else {
+    window.location.href = forwardFunctions[i]
+    }
+}
+
+function infoBox(i) {
+    if (i === 4) {
+    d.querySelector("form").style.display = "none"
+    d.querySelector(".infoModal").style.display = "inherit"
+    } else if (i === 5){
+        d.querySelector("form").style.display = "inherit"
+        d.querySelector(".infoModal").style.display = "none"
+    }
+}
